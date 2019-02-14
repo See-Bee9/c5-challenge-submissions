aef7d987ed2b87ef25ff007a787841774f647d3f
James T. Matta <jamesmatta@gmail.com>
2019-02-14 09:46:31 -0500

    minor bug fix

--- two-egg-problem/KnoxVillains/scripting/twoegg.py
+++ two-egg-problem/KnoxVillains/scripting/twoegg.py
@@ -1,5 +1,5 @@
-import sys,math;t,b,l=int(sys.argv[1]),int(sys.argv[2]),0;s=int(math.sqrt(1+(8*t))/2);r,e=t-((s*(s+1))/2),t+1;p=False;f=s
-while(f<t):
+import sys,math;t,b,l=int(sys.argv[1]),int(sys.argv[2]),0;s=int((math.sqrt(1+(8*t))-1)/2);r,e=t-((s*(s+1))/2),t+1;p=False;f=s
+while(f<=t):
  if f>=b:
   e=f;print f,"\nTrue";break
  print f,"\nFalse";l=f
