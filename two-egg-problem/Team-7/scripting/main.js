21d4c444ba60555294a56e6f10bb77b5f77dadd4
Aaron Pool <aaron.pool@cadre5.com>
2019-02-14 09:31:01 -0500

    Fixed a few logic bugs (oopsie)

--- two-egg-problem/Team-7/scripting/main.js
+++ two-egg-problem/Team-7/scripting/main.js
@@ -2,10 +2,11 @@ export default ([,, z, l]) => {
   const {ceil: c, sqrt: v} = Math;
   const y = c(v(z));
   const x = (s, i) => {
-    for(let f = s; f <= l; f += i) {
+    let f = s + i;
+    for(f; f <= l; f += i) {
       console.log(`${f}\n${f >= l}`)
-      if(f >= l) return f;
     }
+    return f - i;
   }
   const b = x(1, y);
   const a = x(b, 1);
